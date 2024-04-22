dff27132c1cd23648177d454a07036057a820229
phthalos <86087840+phthalos@users.noreply.github.com>
2024-04-23 02:15:33 +0900

    [Bronze II] Title: 상수, Time: 128 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,21 +1,26 @@
 //import java.util.*;
-import java.util.Scanner;
+//import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        
-        /* 
-            StringBuilder는 문자열을 다루는 클래스로, reverse() 메소드를 포함하고 있다.
-            append()를 이용하여 값을 넣고 toString()으로 변환된 문자열은
-            Integer.parseInt()를 통해 int 타입으로 다시 변환된다.
-         */
-        a = Integer.parseInt(new StringBuilder().append(a).reverse().toString());
-        b = Integer.parseInt(new StringBuilder().append(b).reverse().toString());
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        /*  BufferedReader는 한 줄씩 입력을 받기 때문에 StringTokenizer 또는
+            String 배열, split()메소드로 공백을 구분해주어야 한다. */
 
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        /*
+            Scanner를 사용한다면 int타입으로 받아오기 때문에
+            객체를 생성할 때 append() 메소드를 사용해야 한다.
+            하지만 여기서는 넣어주려는 인자가 String 타입이므로 그럴 필요 없이 곧바로 값을 넣어주었다.
+         */
+        int a = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int b = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
         int result = Math.max(a, b);
+
         System.out.print(result);
     }
 }
