b4bf3e3844e6f8028b800fec95845b73db224616
phthalos <86087840+phthalos@users.noreply.github.com>
2024-04-22 22:48:54 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,12 @@
+//import java.util.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        int result = a + b;
+        if (0 < a && b  < 10){System.out.print(result);}
+}}
