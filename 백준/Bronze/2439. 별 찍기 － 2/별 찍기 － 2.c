d10271841d03b05593cde6bf2407c3dc86297f8f
phthalos <86087840+phthalos@users.noreply.github.com>
2024-04-27 22:51:51 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void){
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++){
+        for (int j = i; j < n-1; j++) printf(" ");
+        for (int k = 0; k <= i; k++) printf("*");
+        printf("\n");
+    }
+    return 0;
+}
