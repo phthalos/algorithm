f1bb049cac06b1a2e32a838a9ce475e86637982d
phthalos <86087840+phthalos@users.noreply.github.com>
2024-04-29 01:24:44 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void){
+    int n;
+    scanf("%d",&n);
+        // 0~4번째줄 출력
+    for(int i=0; i<n; i++){
+            for(int j=i; j<n-1; j++){
+                printf(" ");
+            }
+            for(int k=0; k<2*i+1; k++){
+                printf("*");
+            }
+            printf("\n");
+    }
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<i+1; j++){
+            printf(" ");
+        }
+        for(int k=2*(i+1); k<2*n-1; k++){
+            printf("*");
+        }
+        printf("\n");
+
+    }
+    return 0;
+}
