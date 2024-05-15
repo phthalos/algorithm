d9f7892bc3c1ccd2c7d4581f0f614d2cad146427
phthalos <86087840+phthalos@users.noreply.github.com>
2024-05-15 21:52:54 +0900

    [Bronze III] Title: 팩토리얼, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int fact(int n)
+{
+    if(n > 1)
+        return n * fact(n-1);
+    else return 1;
+}
+
+int main(void){
+    int n, res=1;
+    scanf("%d",&n);
+    printf("%d",fact(n));
+    return 0;
+}
