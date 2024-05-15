8826104f209b7a593818e5c5e733cc47be3f7e6f
phthalos <86087840+phthalos@users.noreply.github.com>
2024-05-15 22:07:13 +0900

    [Bronze III] Title: A+B - 6, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void){
+    int n,i,a,b;
+    scanf("%d",&n);
+    for(i=0; i<n; i++)
+    {
+        scanf("%d,%d",&a,&b);
+        printf("%d\n",a+b);
+    }
+}
