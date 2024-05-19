4ec318e66993b328de636a4532e7155410b1a514
phthalos <86087840+phthalos@users.noreply.github.com>
2024-05-20 07:13:40 +0900

    [Bronze V] Title: Serca, Time: 124 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n, i;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        printf(" @@@   @@@ \n@   @ @   @\n@    @    @\n@         @\n @       @ \n  @     @  \n   @   @   \n    @ @    \n     @     \n");
+    }
+    return 0;
+}
