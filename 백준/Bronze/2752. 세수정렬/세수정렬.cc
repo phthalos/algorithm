6269276fbc7d6ece1fe80c744d4c7d3944c06533
phthalos <86087840+phthalos@users.noreply.github.com>
2024-05-30 00:12:25 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.cc
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -1,22 +1,15 @@
 #include <iostream>
+#include <algorithm>
+
 using namespace std;
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int n[3], t, i;
-    cin >> n[0] >> n[1] >> n[2];
-    for (i = 0; i < 3; i++)
-    {
-        for (int j = i; j < 3; j++)
-        {
-            if (n[i] > n[j])
-            {
-                t = n[i];
-                n[i] = n[j];
-                n[j] = t;
-            }
-        }
-    }
-    for (i = 0; i < 3; i++) cout << n[i] << ' ';
+    int a, b, c, d, e, f;
+    cin >> a >> b >> c;
+    d = min({a, b, c});
+    f = max({a, b, c});
+    e = a + b + c - d - f;
+    cout << d << ' ' << e << ' ' << f;
 }
