559a3d81e8e03410fa64fda4622c728fb9725728
phthalos <86087840+phthalos@users.noreply.github.com>
2024-05-31 01:06:21 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <string>
 using namespace std;
+int alphabet[26];
 int main(void)
 {
     ios::sync_with_stdio(0); cin.tie(0);
-    int alphabet[26] = {0}, i;
     string str;
     cin >> str;
-    for (i = 0; i < str.length(); i++)
-        alphabet[str[i] - 97]++;
-    for (i = 0; i < 26; i++)
+    for (auto c : str)
+        alphabet[c - 'a']++;
+    for (int i = 0; i < 26; i++)
         cout << alphabet[i] << ' ';
 }
