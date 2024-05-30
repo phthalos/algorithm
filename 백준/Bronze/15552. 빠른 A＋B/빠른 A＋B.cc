1b8254b002ef3953e46a6386a090e90fa8ab3c5d
phthalos <86087840+phthalos@users.noreply.github.com>
2024-05-30 19:33:19 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 224 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t, a, b;
+    cin >> t;
+    while (t--)
+    {
+        cin >> a >> b;
+        cout << a + b << "\n";
+    }
+}
