f98a1459e5b35c83c6c2c54ea7b0d6052354206f
phthalos <86087840+phthalos@users.noreply.github.com>
2024-05-30 19:44:05 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n, i, j;
+    cin >> n;
+    for (i = 0; i < n; i++)
+    {
+        for (j = i; j < n; j++)
+            cout << "*";
+        cout << "\n";
+    }
+}
