f4b7baf556fbad35bca5b3a5c8c22cb12ef96dc0
phthalos <86087840+phthalos@users.noreply.github.com>
2025-02-21 22:42:38 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int a, b, c, d, e, f;
+
+int main()
+{
+    cin >> a >> b >> c >> d >> e >> f;
+    for (int x = -999; x <= 999; x++)
+    {
+        for (int y = -999; y <= 999; y++)
+        {
+            if (a * x + b * y == c)
+            {
+                if (d * x + e * y == f)
+                {
+                    cout << x << " " << y;
+                    return 0;
+                }
+            }
+        }
+    }
+    return 0;
+}
